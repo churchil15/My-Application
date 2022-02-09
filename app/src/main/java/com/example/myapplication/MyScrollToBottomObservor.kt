772e307526333d6dc6645cc0e6a4a8be592f034e
Churchil Yash Rajpal <85837940+churchil15@users.noreply.github.com>
2022-02-09 20:25:07 +0530

    Update MyScrollToBottomObservor.kt

--- app/src/main/java/com/example/myapplication/MyScrollToBottomObservor.kt
+++ app/src/main/java/com/example/myapplication/MyScrollToBottomObservor.kt
@@ -13,9 +13,8 @@ class MyScrollToBottomObserver(
         super.onItemRangeInserted(positionStart, itemCount)
         val count = adapter.itemCount
         val lastVisiblePosition = manager.findLastCompletelyVisibleItemPosition()
-        // If the recycler view is initially being loaded or the
-        // user is at the bottom of the list, scroll to the bottom
-        // of the list to show the newly added message.
+        
+        
         val loading = lastVisiblePosition == -1
         val atBottom = positionStart >= count - 1 && lastVisiblePosition == positionStart - 1
         if (loading || atBottom) {
